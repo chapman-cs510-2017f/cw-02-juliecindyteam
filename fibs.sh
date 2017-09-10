d42549fc8f5d08cc3099c59ffef77cc8b0745cec
Julie <gardnerh@chapman.edu>
2017-09-10 22:27:18 +0000

    Added fibonacci file

---
+++ fibs.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+echo -n "Enter a positive number :"
+read Num
+if [$Num < 1]
+then
+    echo
+    exit 1
+else
+    f1=1
+    f2=1
+    echo "The Fibonacci sequence is : "
+    for (( i=1;i<=Num;i++ ))
+    do
+         echo -n "$f1 "
+         fn=$((f1+f2))
+         f1=$f2
+         f2=$fn
+	 done
+fi
+
+echo
